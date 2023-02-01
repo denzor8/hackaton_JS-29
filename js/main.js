4c028d580d01c7a7e1ec77a204dd42f2563ad84d
abdrasulovsanat <abdrasulovsanat@gmail.com>
2023-02-01 18:58:14 +0600

    html

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ let closeModalBtn = document.querySelector("#btn-close-modal");
 let studentInpImage = document.querySelector('.student-image');
 // let studentCategory = document.querySelector('#student-category');
 
-let STUDENTS_API = "http://localhost:8000/students";
+let STUDENTS_API = "http://localhost:7000/students";
 
 //create Students
 function createStudent() {
